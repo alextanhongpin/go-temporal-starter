1458332398a6d7c2d3678cd9b4a7d2962787d0ff
alextanhongpin <alextan220990@gmail.com>
2021-06-08 01:00:24 +0800

    chore: add query example

---
+++ hello-world/go.mod
@@ -0,0 +1,8 @@
+module github.com/alextanhongpin/go-temporal-starter/hello-world
+
+go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.0
+	go.temporal.io/sdk v1.6.0
+)
--- hello-world/workflow.go
+++ hello-world/workflow.go
@@ -7,12 +7,24 @@ import (
 )
 
 func GreetingWorkflow(ctx workflow.Context, name string) (string, error) {
+	queryType := "current_state"
+	currentState := "started"
+	err := workflow.SetQueryHandler(ctx, queryType, func() (string, error) {
+		return currentState, nil
+	})
+
+	currentState = "sleeping"
+	if err := workflow.Sleep(ctx, 10*time.Second); err != nil {
+		return "", err
+	}
+	currentState = "woke up"
+
 	options := workflow.ActivityOptions{
 		StartToCloseTimeout: time.Second * 5,
 	}
 	ctx = workflow.WithActivityOptions(ctx, options)
 
 	var result string
-	err := workflow.ExecuteActivity(ctx, ComposeGreeting, name).Get(ctx, &result)
+	err = workflow.ExecuteActivity(ctx, ComposeGreeting, name).Get(ctx, &result)
 	return result, err
 }
